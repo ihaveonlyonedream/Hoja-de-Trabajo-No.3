a8983b80ae74092d4e4d254334525fd4840e2371
nancygm <Nancyg@mbp-de-nancy>
2014-08-03 22:17:08 -0600

    QuickSort de lana!

--- Hoja de Trabajo No.3/src/ht4/Sorting.java
+++ Hoja de Trabajo No.3/src/ht4/Sorting.java
@@ -49,6 +49,40 @@ public class Sorting
 	   insertionSort(elementos, index + 1);
        }
    }
+   
+   /*QuickSort Nancy Girón 
+    Codigo tomado de: Java Structures/Data structures for the principled programmer
+    * Autor: Duanef Bailey*/
+   public static void quicksort(int A[], int izq, int der)
+    {
+
+        int piv=A[izq]; //Tomamos el primer elemento de piv.
+        int i=izq; // i va a buscar de izq a der. 
+        int j=der; // j va a buscar de der a izq.
+        int aux;
+
+        while(i<j)
+        {// Mientras no se cruce la busquda, esto se va a repetir. 
+           while(A[i]<=piv && i<j) i++; // Busca elementos mayores del piv. 
+                while(A[j]>piv) j--;         //Busca un elemento menor que el piv. 
+                     if (i<j) 
+                     {                                        
+                         aux= A[i];           // hacer intercambio.
+                         A[i]=A[j];
+                         A[j]=aux;
+                     }
+                  }
+        /*
+         * El piv sera colocado de manera que del lado izquierdo estaran los menores a el, y 
+         * del lado derecho se pocisionaran los mayores a el.
+        */
+         A[izq]=A[j];               
+         A[j]=piv;                     
+         if(izq<j-1)
+            quicksort(A,izq,j-1); // se ordena el subarray izquierdo
+         if(j+1 <der)
+            quicksort(A,j+1,der); // se ordena el subarray derecho
+    }
 
 }
 
